602c77195cfc11b4db6d49cee07cf05ba74cba0e
marezelej <marlej1997@gmail.com>
2021-12-20 21:54:29 -0300

    Aumentamos la dimensión del vector + pendiente parsear los switch en JS

--- backend/app/Distance/Euclidean/Handler/ChoiceHandler.ts
+++ backend/app/Distance/Euclidean/Handler/ChoiceHandler.ts
@@ -17,3 +17,5 @@ export class IfStatement extends BaseHandler {
     }
   }
 }
+
+// TODO Handle case -> switch
--- backend/app/Distance/Euclidean/MetricVectorsDistance.ts
+++ backend/app/Distance/Euclidean/MetricVectorsDistance.ts
@@ -5,16 +5,50 @@ import {sum} from 'lodash'
 
 export class CodeVector {
   loopCount = 0
-  paramsCount = 0
+  simpleLoopCount = 0 // sin nivel de anidamiento
+  doubleLoopCount = 0 // 1 nivel de anidamiento
+  multipleLoopCount = 0 // 2 o más niveles de anidamiento
+  sumOpCount = 0
+  diffOpCount = 0
+  divOpCount = 0
+  prodOpCount = 0
+  concatOpCount = 0
+  paramsCount = 0 // Tiene más importancia, podríamos dar más peso (5 por ejemplo)
   maxTreeDeep = 0
   ifCount = 0
+  simpleIfCount = 0 // sin nivel de anidamiento
+  doubleIfCount = 0 // 1 nivel de anidamiento
+  multipleIfCount = 0 // 2 o más niveles de anidamiento
+  hasArrays = 0 // Utilizar un peso para darle más importancia, como 10 por ejemplo
 }
 
 export default class MetricVectorsDistance {
   async distance(a: string, b: string): Promise<number> {
     const aVector = Object.values(this.toVector(a))
     const bVector = Object.values(this.toVector(b))
-    return sum(aVector.map((aValue, key) => Math.abs(aValue - bVector[key])))
+    const wVector = Object.values(this.getWeightVector())
+    return sum(aVector.map((aValue, key) => Math.abs(aValue - bVector[key]) * wVector[key]))
+  }
+
+  protected getWeightVector(): CodeVector {
+    return {
+      loopCount: 1,
+      simpleLoopCount: 1,
+      doubleLoopCount: 1,
+      multipleLoopCount: 1,
+      sumOpCount: 1,
+      diffOpCount: 1,
+      divOpCount: 1,
+      prodOpCount: 1,
+      concatOpCount: 1,
+      paramsCount: 1,
+      maxTreeDeep: 1,
+      ifCount: 1,
+      simpleIfCount: 1,
+      doubleIfCount: 1,
+      multipleIfCount: 1,
+      hasArrays: 10,
+    }
   }
 
   public toVector(code: string): CodeVector {
