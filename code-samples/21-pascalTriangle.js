908a382f028e48d6478cd9a1dd8f47b71e0385a9
marezelej <marlej1997@gmail.com>
2022-01-11 01:09:47 -0300

    Fix sample functions

--- code-samples/21-pascalTriangle.js
+++ code-samples/21-pascalTriangle.js
@@ -11,29 +11,11 @@ function pascalTriangle(lineNumber) {
     return currentLine;
 }
 
-function pascalTriangleRecursive(lineNumber) {
-    if (lineNumber === 0) {
-        return [1];
+function pascalTriangle2(lineNumber) {
+    let currentLine = [1];
+    for (let numIndex = 1; numIndex < (lineNumber + 1); numIndex += 1) {
+        const previousLine = currentLine[numIndex - 1];
+        currentLine[numIndex] = (previousLine * (lineNumber - numIndex + 1)) / numIndex;
     }
-
-    const currentLineSize = lineNumber + 1;
-    const previousLineSize = currentLineSize - 1;
-
-    // Create container for current line values.
-    const currentLine = [];
-
-    // We'll calculate current line based on previous one.
-    const previousLine = pascalTriangleRecursive(lineNumber - 1);
-
-    // Let's go through all elements of current line except the first and
-    // last one (since they were and will be filled with 1's) and calculate
-    // current coefficient based on previous line.
-    for (let numIndex = 0; numIndex < currentLineSize; numIndex += 1) {
-        const leftCoefficient = (numIndex - 1) >= 0 ? previousLine[numIndex - 1] : 0;
-        const rightCoefficient = numIndex < previousLineSize ? previousLine[numIndex] : 0;
-
-        currentLine[numIndex] = leftCoefficient + rightCoefficient;
-    }
-
     return currentLine;
 }
