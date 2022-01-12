2d2b702adab25508be84edc6d4600899b3d7a720
FacundoFederico <facufede1998@gmail.com>
2022-01-12 20:45:33 -0300

    fix: fixes original functions

--- code-samples/30-cartesian-product.js
+++ code-samples/30-cartesian-product.js
@@ -1,23 +1,23 @@
 function cartesianProduct(setA, setB) {
-    // Check if input sets are not empty.
-    // Otherwise return null since we can't generate Cartesian Product out of them.
-    if (!setA || !setB || !setA.length || !setB.length) {
-        return null;
+  // Check if input sets are not empty.
+  // Otherwise return null since we can't generate Cartesian Product out of them.
+  if (!setA || !setB || !setA.length || !setB.length) {
+    return null;
+  }
+
+  // Init product set.
+  const product = [];
+
+  // Now, let's go through all elements of a first and second set and form all possible pairs.
+  for (let indexA = 0; indexA < setA.length; indexA += 1) {
+    for (let indexB = 0; indexB < setB.length; indexB += 1) {
+      // Add current product pair to the product set.
+      product.push([setA[indexA], setB[indexB]]);
     }
+  }
 
-    // Init product set.
-    const product = [];
-
-    // Now, let's go through all elements of a first and second set and form all possible pairs.
-    for (let indexA = 0; indexA < setA.length; indexA += 1) {
-        for (let indexB = 0; indexB < setB.length; indexB += 1) {
-            // Add current product pair to the product set.
-            product.push([setA[indexA], setB[indexB]]);
-        }
-    }
-
-    // Return cartesian product set.
-    return product;
+  // Return cartesian product set.
+  return product;
 }
 
 function cartesianProduct(set1, set2) {
--- code-samples/38-power-set-recursive.js
+++ code-samples/38-power-set-recursive.js
@@ -1,27 +1,27 @@
 function btPowerSetRecursive(originalSet, allSubsets = [[]], currentSubSet = [], startAt = 0) {
-    // Let's iterate over originalSet elements that may be added to the subset
-    // without having duplicates. The value of startAt prevents adding the duplicates.
-    for (let position = startAt; position < originalSet.length; position += 1) {
-      // Let's push current element to the subset
-      currentSubSet.push(originalSet[position]);
-  
-      // Current subset is already valid so let's memorize it.
-      // We do array destruction here to save the clone of the currentSubSet.
-      // We need to save a clone since the original currentSubSet is going to be
-      // mutated in further recursive calls.
-      allSubsets.push([...currentSubSet]);
-  
-      // Let's try to generate all other subsets for the current subset.
-      // We're increasing the position by one to avoid duplicates in subset.
-      btPowerSetRecursive(originalSet, allSubsets, currentSubSet, position + 1);
-  
-      // BACKTRACK. Exclude last element from the subset and try the next valid one.
-      currentSubSet.pop();
-    }
-  
-    // Return all subsets of a set.
-    return allSubsets;
+  // Let's iterate over originalSet elements that may be added to the subset
+  // without having duplicates. The value of startAt prevents adding the duplicates.
+  for (let position = startAt; position < originalSet.length; position += 1) {
+    // Let's push current element to the subset
+    currentSubSet.push(originalSet[position]);
+
+    // Current subset is already valid so let's memorize it.
+    // We do array destruction here to save the clone of the currentSubSet.
+    // We need to save a clone since the original currentSubSet is going to be
+    // mutated in further recursive calls.
+    allSubsets.push([...currentSubSet]);
+
+    // Let's try to generate all other subsets for the current subset.
+    // We're increasing the position by one to avoid duplicates in subset.
+    btPowerSetRecursive(originalSet, allSubsets, currentSubSet, position + 1);
+
+    // BACKTRACK. Exclude last element from the subset and try the next valid one.
+    currentSubSet.pop();
   }
+
+  // Return all subsets of a set.
+  return allSubsets;
+}
 
   function btPowerSetRecursive(originalSet, allSubsets = [[]], currentSubSet = [], startAt = 0) {
     let position = startAt;
